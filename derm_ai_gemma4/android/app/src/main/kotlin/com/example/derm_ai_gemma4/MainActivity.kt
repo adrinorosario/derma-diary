680d4a5a6788caf8419be520e2194e70ae1dc0a1
Adrino Rosario <rosarioadrino@gmail.com>
2026-04-30 12:23:10 +0530

    refactored entire codebase

--- derm_ai_gemma4/android/app/src/main/kotlin/com/example/derm_ai_gemma4/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.derm_ai_gemma4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ dermai_gemma4/android/app/src/main/kotlin/com/example/dermai_gemma4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dermai_gemma4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
